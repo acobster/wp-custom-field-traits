ff89d49435612bff44b2f723f9eab69fb8a8b3dc
The Ambassador of Funk <acobster@gmail.com>
2016-01-24 11:57:31 -0800

    simplify TestCase\Base

--- test/test-lib/TestCase/Base.php
+++ test/test-lib/TestCase/Base.php
@@ -10,21 +10,12 @@ use \Cft\Plugin;
  * complain about a lack of tests defined here.
  */
 abstract class Base extends \PHPUnit_Framework_TestCase {
-  protected $plugin;
-
   public function setUp() {
     WP::setUp();
-    $this->plugin = $this->configureCft( \Cft\Plugin::getInstance() );
   }
 
   public function tearDown() {
     WP::tearDown();
-    unset($this->plugin);
-  }
-
-
-  protected function configureCft( $plugin ) {
-    return $plugin;
   }
 }
 
