321c8702ebc05f29453fc409b3d93c0beb74aa44
Coby Tamayo <acobster@gmail.com>
2019-02-05 21:19:29 -0800

    update PHPUnit

--- test/test-lib/TestCase/Base.php
+++ test/test-lib/TestCase/Base.php
@@ -4,12 +4,13 @@ namespace CftTest\TestCase;
 
 use \WP_Mock as WP;
 use \Cft\Plugin;
+use PHPUnit\Framework\TestCase;
 
 /**
  * Base test class for the plugin. Declared abstract so that PHPUnit doesn't
  * complain about a lack of tests defined here.
  */
-abstract class Base extends \PHPUnit_Framework_TestCase {
+abstract class Base extends TestCase {
   protected $plugin;
 
   public function setUp() {
